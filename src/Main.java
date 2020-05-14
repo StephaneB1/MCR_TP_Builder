152ea00de36f6763a502e0ee57bd83c2b1ed8bf9
Juri <Chau-ying-kot@hotmail.com>
2020-05-14 15:48:33 +0200

    comment builder test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import carBuilder.CarBuilder;
 import cars.Body;
 import cars.Car;
 import cars.Motor;
+import cars.Spoiler;
 
 import java.awt.*;
 
@@ -10,12 +11,13 @@ public class Main {
     public static void main(String[] args) {
         new Controller();
 
-        Car builder = Controller.createNewCar("test", Color.RED)
+       /* Car builder = Controller.createNewCar("test", Color.RED)
                 .buildBody(new Body("Body", "bodyTemplate.png", 1, 1, 1, 1, 11))
                 .buildMotor(new Motor("Motor", "motorTemplate.png",1,1,1,1,1,new Point()))
+                .buildSpoiler(new Spoiler("Motor", "spoilerTemplate.png",1,1,1,1,1,new Point()))
                 .getCar();
 
-        builder.getAcceleration();
+        builder.getAcceleration();*/
 
     }
 
