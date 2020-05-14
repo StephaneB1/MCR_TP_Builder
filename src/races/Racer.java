0c19410a96f58b8ed5712cf9c0c6bfd381442be5
Robin <r.demarta@outlook.com>
2020-05-14 16:16:47 +0200

    Racer WIP: added basic crash event

--- src/races/Racer.java
+++ src/races/Racer.java
@@ -7,14 +7,17 @@ import java.util.Random;
 
 public class Racer {
     private Car car;
-    private double currentDistance;
+    private double currentDistanceKm;
     private String name;
     private boolean displayLogs;
 
     // Store stats to avoid unnecessary recalculations
-    final double CRASH_CHANCE; // [0;1] Depends on car's adherence and maniability
-    final double SPEED_FACTOR;  // [0;1]
-    final double RESISTANCE_FACTOR; // [0;1]
+    // Factors and chances: [0;1]
+    private final double CRASH_CHANCE; // Depends on car's adherence and maniability
+    private final double SPEED; // Racer's speed affected by Car's stats
+    private final double RESISTANCE_FACTOR;
+
+    private final static double CRASH_DISTANCE_KM = 0.1; // Base distance in km that the Racer looses if it crashes
 
     public Racer(String name, Car car, boolean displayLogs) {
         this.name = name;
@@ -23,16 +26,24 @@ public class Racer {
 
         // Calculate stats
         CRASH_CHANCE = 1 - Utils.average(car.getAdherence(), car.getManiability());
-        SPEED_FACTOR = Car.getBaseSpeedKmh() * car.getAcceleration();
+        SPEED = Car.getBaseSpeedKmh() * car.getAcceleration();
         RESISTANCE_FACTOR = car.getResistance();
     }
 
     /**
-     * Simple distance getter.
-     * @return the current distance the Racer has reached
+     * The total distance the Racer has ran so far.
+     * @return the distance in kilometer
      */
-    public double getCurrentDistance() {
-        return currentDistance;
+    public double getCurrentDistanceKilometer() {
+        return currentDistanceKm;
+    }
+
+    /**
+     * The total distance the Racer has ran so far.
+     * @return the distance in meter
+     */
+    public double getCurrentDistanceMeter() {
+        return getCurrentDistanceKilometer() / 1000;
     }
 
     /**
@@ -43,21 +54,25 @@ public class Racer {
         return name;
     }
 
-    /**
-     *
-     * @return the distance the Racer has ran this tick
-     */
-    public double runOneTick() {
-        double runDistance = 0;
+    public double runOneTick(double tickValueHour) {
+        // The tick must have a positive temporal value
+        if(tickValueHour < 0)
+            throw new RuntimeException("Cannot run for a negative amount of time.");
+
+        double runDistanceKm = tickValueHour * SPEED; // Initialize to the distance the Racer may run at maximum;
         Random rand = new Random();
 
-        // Car may crash
+        // Car may crash => apply distance malus
         if(rand.nextDouble() < CRASH_CHANCE) {
-            displayLog("The car crashed");
+            // The Racer's resistance softens the crash
+            double crashMalusDistanceKm = CRASH_DISTANCE_KM * (1 - RESISTANCE_FACTOR);
+            runDistanceKm -= crashMalusDistanceKm;
+            displayLog(name + " crashed: -" + crashMalusDistanceKm + "Km");
         }
 
-        currentDistance += runDistance;
-        return runDistance;
+        currentDistanceKm += runDistanceKm;
+        displayLog(name + " ran " + runDistanceKm + "Km");
+        return runDistanceKm;
     }
 
     private void displayLog(String message) {
