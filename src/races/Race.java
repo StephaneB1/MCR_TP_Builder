caf3c05d4107c354fb56610a9c58b426b5b5965b
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-05-22 12:31:47 +0200

    Add winner winner chicken dinner popup

--- src/races/Race.java
+++ src/races/Race.java
@@ -62,22 +62,23 @@ public class Race extends JFrame implements WindowListener {
             public void run() {
 
                 while(isRunning){
-                    System.out.println(racers.get(1).getCurrentDistanceMeter());
-                    // Check if there is a winner
-                    for(Racer racer : racers){
-                        if(racer.getCurrentDistanceMeter() >= totalDistance){
-                            isRunning = false;
-                            displayWinner();
-                            break;
-                        }
-                    }
 
-                    // there is no winner -> run one tick
+                    racePanel.repaint();
+
+                    // Run a tick
                     for(Racer racer : racers){
                         racer.runOneTick(2);
                     }
 
-                    racePanel.repaint();
+                    // Check if there is a winner
+                    for(Racer racer : racers){
+                        if(racer.getCurrentDistanceMeter() >= totalDistance){
+                            isRunning = false;
+                            displayWinner(racer);
+                            break;
+                        }
+                    }
+
 
                     try {
                         Thread.sleep(500);
@@ -92,11 +93,6 @@ public class Race extends JFrame implements WindowListener {
         long period = 10;
         timer.scheduleAtFixedRate(repeatedTask, delay, period);
 
-
-
-
-
-
     }
 
     public void stop() {
@@ -107,8 +103,8 @@ public class Race extends JFrame implements WindowListener {
 
     }
 
-    private void displayWinner() {
-
+    private void displayWinner(Racer racerWinner) {
+        JOptionPane.showMessageDialog(null, racerWinner.getName() + " won the race !", "winner winner chicken dinner", JOptionPane.INFORMATION_MESSAGE);
     }
 
     @Override
