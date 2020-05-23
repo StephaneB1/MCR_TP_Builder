0f15cc67fe9dbef5bb7ff3435fb4390c207b6a80
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-05-23 17:37:11 +0200

    Jpanels reordering

--- src/Controller.java
+++ src/Controller.java
@@ -21,200 +21,68 @@ public class Controller extends JFrame {
     private final int SCREEN_WIDTH  = 1200;
     private final int SCREEN_HEIGHT = 600;
 
-    private final Color CAR_STATS_BG_COLOR     = Color.WHITE;
-    private final Color SHOP_AND_RACE_BG_COLOR = Color.WHITE; //new Color(230, 230, 230);
+    // Car stats
+    private final JLabel accelerationLabel = new JLabel();
+    private final JLabel weightLabel       = new JLabel();
+    private final JLabel adherenceLabel    = new JLabel();
+    private final JLabel maniabilityLabel  = new JLabel();
+    private final JLabel resistanceLabel   = new JLabel();
 
     private Garage garage;
     private int currentCategory;
     private int currentProduct;
     private Car playerCar;
 
+
     public Controller() {
 
         setupGarage();
-        playerCar = new Car();
 
         setTitle("MCR - Racers");
 
-        // Car frame with statistics
-        JPanel carPanel = new JPanel();
-        carPanel.setBackground(CAR_STATS_BG_COLOR);
-        carPanel.setLayout(new GridLayout(1,2));
-        // -- Player car panel
-        JPanel carPanelImage = playerCar;
-        carPanelImage.setOpaque(false);
-        carPanel.add(carPanelImage);
-        // -- Player car stat's panel
-        JPanel carStatsPanel = new JPanel();
-        carStatsPanel.setOpaque(false);
-        carStatsPanel.setLayout(new GridLayout(5, 2));
-        final JLabel accelerationLabel = new JLabel(playerCar.getAcceleration() + "m/s2");
-        final JLabel weightLabel       = new JLabel(playerCar.getWeight() + "kg");
-        final JLabel adherenceLabel    = new JLabel(playerCar.getAdherence() + "/10");
-        final JLabel maniabilityLabel  = new JLabel(playerCar.getManiability() + "/10");
-        final JLabel resistanceLabel   = new JLabel(playerCar.getResistance() + "%");
-        carStatsPanel.add(new JLabel("acceleration :"));
-        carStatsPanel.add(accelerationLabel);
-        carStatsPanel.add(new JLabel("weight :"));
-        carStatsPanel.add(weightLabel);
-        carStatsPanel.add(new JLabel("adherence :"));
-        carStatsPanel.add(adherenceLabel);
-        carStatsPanel.add(new JLabel("maniability :"));
-        carStatsPanel.add(maniabilityLabel);
-        carStatsPanel.add(new JLabel("resistance :"));
-        carStatsPanel.add(resistanceLabel);
-        carPanel.add(carStatsPanel);
+        // General app layout (mainPanel) :
+        /*---------------------------------------------*
+         * MCR - Racers                                *
+         *---------------------------------------------*
+         * BLUEPRINTS            | STATS               *
+         * - Car preview         | - Car performances  *
+         *                       |                     *
+         *=============================================*
+         * BUILDER               | RACE                *
+         * - Car components      | - Race information  *
+         *                       |                     *
+         *---------------------------------------------*/
+        setBackground(Color.GREEN);
 
-        // Builder and race UI
-        JPanel builderAndRacePanel = new JPanel();
-        builderAndRacePanel.setBackground(SHOP_AND_RACE_BG_COLOR);
-        GridLayout builderRaceLayout = new GridLayout(1, 2);
-        builderRaceLayout.setHgap(50);
-        builderAndRacePanel.setLayout(builderRaceLayout);
-        // -- Builder panel (carPart images + selection interface)
-        JPanel builderPanel = new JPanel();
-        builderPanel.setOpaque(false);
-        builderPanel.setLayout(new GridLayout(2, 1));
-        JPanel carPartPanel = new JPanel();
-        carPartPanel.setOpaque(false);
-        JPanel selectionPanel = new JPanel();
-        selectionPanel.setOpaque(false);
-        selectionPanel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        // -- Selection labels
-        JLabel categoryLabel = new JLabel(garage.getInventory().get(currentCategory).getProductLabel());
-        categoryLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        JLabel productLabel = new JLabel(garage.getInventory().get(currentCategory).getProducts().get(currentProduct).getName());
-        JButton categoryLeftButton = new JButton("<");
-        categoryLeftButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentCategory = (currentCategory - 1 + garage.getInventory().size()) % garage.getInventory().size();
-                currentProduct  = 0;
-                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
-            }
-        });
-        JButton categoryRightButton = new JButton(">");
-        categoryRightButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentCategory = (currentCategory + 1) % garage.getInventory().size();
-                currentProduct  = 0;
-                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
-            }
-        });
-        JButton productLeftButton = new JButton("<");
-        productLeftButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentProduct = (currentProduct - 1 + garage.getInventory().get(currentCategory).getProducts().size())
-                        % garage.getInventory().get(currentCategory).getProducts().size();
-                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
-            }
-        });
-        JButton productRightButton = new JButton(">");
-        productRightButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentProduct = (currentProduct + 1) % garage.getInventory().get(currentCategory).getProducts().size();
-                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
-            }
-        });
-        JButton mountToCarButton = new JButton("Mount to car");
-        JButton buildCarButton   = new JButton("Build car");
-        mountToCarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Add car part to the player's car
-                playerCar.installCarPart(garage.getInventory().get(currentCategory).getProducts().get(currentProduct));
-                carPanelImage.repaint();
+        JPanel mainPanel = new JPanel();
+        mainPanel.setOpaque(false);
+        GridLayout mainLayout = new GridLayout(2, 2);
+        mainLayout.setHgap(20);
+        mainLayout.setVgap(20);
+        mainPanel.setLayout(mainLayout);
 
-                // Update the stats labels
-                accelerationLabel.setText(playerCar.getAcceleration() + "m/s2");
-                weightLabel.setText(playerCar.getWeight() + "kg");
-                adherenceLabel.setText(playerCar.getAdherence() + "/10");
-                maniabilityLabel.setText(playerCar.getManiability() + "/10");
-                resistanceLabel.setText(playerCar.getResistance() + "%");
-            }
-        });
-        buildCarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                // Build car
-            }
-        });
-        productLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        c.insets = new Insets(3,3,3,3); // padding
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.anchor=GridBagConstraints.CENTER;
-        c.gridy = 0;
-        c.gridx = 0;
-        c.weightx = 0.2;
-        selectionPanel.add(categoryLeftButton, c);
-        c.gridx = 1;
-        c.weightx = 0.6;
-        selectionPanel.add(categoryLabel, c);
-        c.gridx = 2;
-        c.weightx = 0.2;
-        selectionPanel.add(categoryRightButton, c);
-        c.gridy = 1;
-        c.gridx = 0;
-        c.weightx = 0.2;
-        selectionPanel.add(productLeftButton, c);
-        c.gridx = 1;
-        c.weightx = 0.6;
-        selectionPanel.add(productLabel, c);
-        c.gridx = 2;
-        c.weightx = 0.2;
-        selectionPanel.add(productRightButton, c);
-        c.gridy = 2;
-        c.gridx = 1;
-        c.ipady = 30; // make the button bigger
-        selectionPanel.add(mountToCarButton, c);
-        c.gridy = 2;
-        c.gridx = 2;
-        selectionPanel.add(buildCarButton, c);
-        builderPanel.add(carPartPanel);
-        builderPanel.add(selectionPanel);
-
-        // Race info panel
-        JPanel racePanel = new JPanel();
-        racePanel.setOpaque(false);
-        racePanel.setLayout(new GridLayout(3, 2));
-        JButton quitButton = new JButton("Quit");
-        quitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exit(0);
-            }
-        });
-        JButton startButton = new JButton("Start");
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startRace();
-            }
-        });
-        racePanel.add(new JLabel("Racers :"));
-        racePanel.add(new JLabel("5"));
-        racePanel.add(new JLabel("Total distance :"));
-        racePanel.add(new JLabel("5km"));
-        racePanel.add(quitButton);
-        racePanel.add(startButton);
-
-        builderAndRacePanel.add(builderPanel);
-        builderAndRacePanel.add(racePanel);
-
-        GridLayout gl = new GridLayout(2, 1);
-        gl.setVgap(10);
-        setLayout(gl);
+        // BLUEPRINTS
+        playerCar = new Car();
+        playerCar.setOpaque(false);
+        // STATS
+        JPanel carStatsPanel = loadCarStatsPanel();
+        carStatsPanel.setBackground(Color.WHITE);
+        // BUILDER
+        JPanel builderPanel = loadBuilderPanel();
+        builderPanel.setBackground(Color.WHITE);
+        // RACE
+        JPanel racePanel = loadRacePanel();
+        racePanel.setBackground(Color.WHITE);
 
         // Add panels with padding
         Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-        carPanel.setBorder(padding);
-        builderAndRacePanel.setBorder(padding);
-        add(carPanel);
-        add(builderAndRacePanel);
+        mainPanel.setBorder(padding);
+        mainPanel.add(playerCar);
+        mainPanel.add(carStatsPanel);
+        mainPanel.add(builderPanel);
+        mainPanel.add(racePanel);
+
+        add(mainPanel);
 
         //Resizing frame
         setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
@@ -297,4 +165,166 @@ public class Controller extends JFrame {
         return new CarBuilder();
     }
 
+    /*----------------------------------------------------------------*
+     *                      JAVA SWING PANELS                         *
+     *----------------------------------------------------------------*/
+    private JPanel loadCarStatsPanel() {
+        JPanel carStatsPanel = new JPanel();
+        carStatsPanel.setOpaque(true);
+        carStatsPanel.setLayout(new GridLayout(5, 2));
+        accelerationLabel.setText(playerCar.getAcceleration() + "m/s2");
+        weightLabel.setText(playerCar.getWeight() + "kg");
+        adherenceLabel.setText(playerCar.getAdherence() + "/10");
+        maniabilityLabel.setText(playerCar.getManiability() + "/10");
+        resistanceLabel.setText(playerCar.getResistance() + "%");
+        carStatsPanel.add(new JLabel("acceleration :"));
+        carStatsPanel.add(accelerationLabel);
+        carStatsPanel.add(new JLabel("weight :"));
+        carStatsPanel.add(weightLabel);
+        carStatsPanel.add(new JLabel("adherence :"));
+        carStatsPanel.add(adherenceLabel);
+        carStatsPanel.add(new JLabel("maniability :"));
+        carStatsPanel.add(maniabilityLabel);
+        carStatsPanel.add(new JLabel("resistance :"));
+        carStatsPanel.add(resistanceLabel);
+        return carStatsPanel;
+    }
+
+    private JPanel loadBuilderPanel() {
+        JPanel builderPanel = new JPanel();
+        builderPanel.setOpaque(true);
+        builderPanel.setLayout(new GridLayout(2, 1));
+        JPanel carPartPanel = new JPanel();
+        carPartPanel.setOpaque(false);
+        JPanel selectionPanel = new JPanel();
+        selectionPanel.setOpaque(false);
+        selectionPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        // -- Selection labels
+        JLabel categoryLabel = new JLabel(garage.getInventory().get(currentCategory).getProductLabel());
+        categoryLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel productLabel = new JLabel(garage.getInventory().get(currentCategory).getProducts().get(currentProduct).getName());
+        JButton categoryLeftButton = new JButton("<");
+        categoryLeftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentCategory = (currentCategory - 1 + garage.getInventory().size()) % garage.getInventory().size();
+                currentProduct  = 0;
+                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
+            }
+        });
+        JButton categoryRightButton = new JButton(">");
+        categoryRightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentCategory = (currentCategory + 1) % garage.getInventory().size();
+                currentProduct  = 0;
+                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
+            }
+        });
+        JButton productLeftButton = new JButton("<");
+        productLeftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentProduct = (currentProduct - 1 + garage.getInventory().get(currentCategory).getProducts().size())
+                        % garage.getInventory().get(currentCategory).getProducts().size();
+                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
+            }
+        });
+        JButton productRightButton = new JButton(">");
+        productRightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentProduct = (currentProduct + 1) % garage.getInventory().get(currentCategory).getProducts().size();
+                updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
+            }
+        });
+        JButton mountToCarButton = new JButton("Add to blueprint");
+        JButton buildCarButton   = new JButton("Build car");
+        mountToCarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Add car part to the player's car
+                playerCar.installCarPart(garage.getInventory().get(currentCategory).getProducts().get(currentProduct));
+                playerCar.repaint();
+
+                // Update the stats labels
+                accelerationLabel.setText(playerCar.getAcceleration() + "m/s2");
+                weightLabel.setText(playerCar.getWeight() + "kg");
+                adherenceLabel.setText(playerCar.getAdherence() + "/10");
+                maniabilityLabel.setText(playerCar.getManiability() + "/10");
+                resistanceLabel.setText(playerCar.getResistance() + "%");
+            }
+        });
+        buildCarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // Build car
+            }
+        });
+        productLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        c.insets = new Insets(3,3,3,3); // padding
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor=GridBagConstraints.CENTER;
+        c.gridy = 0;
+        c.gridx = 0;
+        c.weightx = 0.2;
+        selectionPanel.add(categoryLeftButton, c);
+        c.gridx = 1;
+        c.weightx = 0.6;
+        selectionPanel.add(categoryLabel, c);
+        c.gridx = 2;
+        c.weightx = 0.2;
+        selectionPanel.add(categoryRightButton, c);
+        c.gridy = 1;
+        c.gridx = 0;
+        c.weightx = 0.2;
+        selectionPanel.add(productLeftButton, c);
+        c.gridx = 1;
+        c.weightx = 0.6;
+        selectionPanel.add(productLabel, c);
+        c.gridx = 2;
+        c.weightx = 0.2;
+        selectionPanel.add(productRightButton, c);
+        c.gridy = 2;
+        c.gridx = 1;
+        c.ipady = 30; // make the button bigger
+        selectionPanel.add(mountToCarButton, c);
+        c.gridy = 2;
+        c.gridx = 2;
+        updateSelectionLabels(categoryLabel, productLabel, carPartPanel);
+        selectionPanel.add(buildCarButton, c);
+        builderPanel.add(carPartPanel);
+        builderPanel.add(selectionPanel);
+
+        return builderPanel;
+    }
+
+    private JPanel loadRacePanel() {
+        JPanel racePanel = new JPanel();
+        racePanel.setOpaque(true);
+        racePanel.setLayout(new GridLayout(3, 2));
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exit(0);
+            }
+        });
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startRace();
+            }
+        });
+        racePanel.add(new JLabel("Racers :"));
+        racePanel.add(new JLabel("5"));
+        racePanel.add(new JLabel("Total distance :"));
+        racePanel.add(new JLabel("5km"));
+        racePanel.add(quitButton);
+        racePanel.add(startButton);
+
+        return racePanel;
+    }
 }
