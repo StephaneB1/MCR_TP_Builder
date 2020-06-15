76ab631b55bb93ec0154644a3c766711a669e900
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-06-15 17:54:13 +0200

    Car comments

--- src/cars/Car.java
+++ src/cars/Car.java
@@ -4,6 +4,10 @@ import cars.parts.CarPart;
 
 import java.util.ArrayList;
 
+/**
+ * MCR PROJECT : Builder Design Pattern
+ * Author      : Bottin Stéphane, Demarta Robin, Dessaules Loïc, Kot Chau-Ying
+ */
 public class Car {
 
     private ArrayList<CarPart> carParts;
@@ -18,20 +22,35 @@ public class Car {
         setCarParts(carParts);
     }
 
+    /**
+     * Set the car parts and update the stats
+     * @param carParts : car parts to add
+     */
     public void setCarParts(ArrayList<CarPart> carParts) {
         this.carParts = carParts;
         updateStats();
     }
 
+    /**
+     * Get the car parts
+     * @return car parts of the car
+     */
     public ArrayList<CarPart> getCarParts() {
         return carParts;
     }
 
+    /**
+     * Get the stats
+     * @return stats
+     */
     public Stats getStats() {
         return stats;
     }
 
-    public void updateStats() {
+    /**
+     * Update the stats depending on the car's car parts
+     */
+    private void updateStats() {
         Stats.updateCarPartStats(stats, carParts);
     }
 
